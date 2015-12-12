c31afc00a8399fb41777d1af3c664f098628a734
anneananda <anne.ananda1@gmail.com>
2015-12-12 07:44:43 -0500

    anneananda_www105_assignment 2

---
+++ anneananda-www105_assignment2/js/plugins.js
@@ -0,0 +1,60 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+
+$(document).ready(function () {
+    // Lightbox Options
+    lightbox.option({
+        'positionFromTop': 150
+    });
+
+    // Initialize ScrollIt
+    $(function(){
+      $.scrollIt();
+    });
+    
+    // Initialize Chosen
+    $('.chosen').chosen();
+    
+    // Initialize Masonry
+    $('.grid').masonry({
+        'itemSelector':'.grid-item',
+        'columnWidth': 0
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
